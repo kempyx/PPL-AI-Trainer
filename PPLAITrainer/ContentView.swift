da3228359e34a4b37c32f90ee7fbeb6b50b227d3
Kemp Calalo <kemp.calalo@primendro.com>
2026-02-16 17:26:08 +0100

    Initial Commit

---
+++ PPLAITrainer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PPLAITrainer
+//
+//  Created by Kemp Calalo on 2026-02-16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PPLAITrainer/PPLAITrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PPLAITrainerApp.swift
+//  PPLAITrainer
+//
+//  Created by Kemp Calalo on 2026-02-16.
+//
+
+import SwiftUI
+
+@main
+struct PPLAITrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
